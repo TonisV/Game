7ad1b67bd77d7be2231cd6503953aacc4a007df2
Tõnis Vaimann <tonisv@tlu.ee>
2018-02-28 19:23:27 +0200

    Lisatud pall ja füüsika, parandatud playeri collaiderid ja füüsika. Lisatud prefab player

--- Game - RIF16/Assets/Scripts/Player/PhisicObject.cs
+++ Game - RIF16/Assets/Scripts/Player/PhisicObject.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PhisicObject : MonoBehaviour {
+public class PhisicObject : MonoBehaviour
+{
 
     public float minGroundNormalY = .65f;
     public float gravityModifier = 1f;
