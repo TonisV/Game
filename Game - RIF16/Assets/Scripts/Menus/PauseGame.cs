9686adf94122f30847bc1836f629415a39eafbdc
Unknown <ristotakk@hotmail.com>
2018-03-05 19:31:33 +0200

    Lisatud main menu ja pause menu

---
+++ Game - RIF16/Assets/Scripts/Menus/PauseGame.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseGame : MonoBehaviour {
+    public Transform canvas;
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Escape)) {
+            Pause();
+        }
+	}
+
+    public void Pause() {
+        GameObject FirstChild = canvas.gameObject.transform.GetChild(0).gameObject;
+        GameObject SecondChild = canvas.gameObject.transform.GetChild(1).gameObject;
+        GameObject ThirdChild = canvas.gameObject.transform.GetChild(2).gameObject;
+
+        if (canvas.gameObject.activeInHierarchy == false) {
+            canvas.gameObject.SetActive(true);
+            Time.timeScale = 0;
+        } else {
+            if (SecondChild.activeInHierarchy == true) {
+                SecondChild.SetActive(false);
+                FirstChild.SetActive(true);
+            } else if (ThirdChild.activeInHierarchy == true) {
+                ThirdChild.SetActive(false);
+                FirstChild.SetActive(true);
+            } else {
+                canvas.gameObject.SetActive(false);
+                Time.timeScale = 1;
+            }
+        }
+    }
+}
