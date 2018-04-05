c5de8242c1f73113f3265967e3f5138517ba50a9
Unknown <ristotakk@hotmail.com>
2018-04-05 20:28:45 +0300

    Lisatud elud ja orad

---
+++ Game - RIF16/Assets/Scripts/Menus/HUD.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUD : MonoBehaviour {
+
+    private Text HealthText;
+
+    // Use this for initialization
+    void Start () {
+        HealthText = GameObject.Find("HealthText").GetComponent<Text>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        HealthText.text = "Health " + PlayerController.curHealth + "/5";
+    }
+}
