33615824c2d991eefe67281f79e27d562a8aa530
Unknown <ristotakk@hotmail.com>
2018-04-05 22:27:23 +0300

    Portaali funktsionaalsus
    Kui player on portaali ees siis laeb järgmise scene. Testimiseks lisatud scene ProtoTV2.

--- Game - RIF16/Assets/Scripts/Menus/PauseGame.cs
+++ Game - RIF16/Assets/Scripts/Menus/PauseGame.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseGame : MonoBehaviour {
     public Transform canvas;
@@ -32,5 +33,9 @@ public class PauseGame : MonoBehaviour {
                 Time.timeScale = 1;
             }
         }
+    }
+
+    public void Restart() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
---
+++ Game - RIF16/Assets/Scripts/PortalTeleport.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PortalTeleport : MonoBehaviour {
+
+    private PlayerController playerController;
+    private GameObject player;
+    private int activeSceneIndex;
+    private string test;
+
+    // Use this for initialization
+    void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerController = player.GetComponent<PlayerController>();
+        activeSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        test = "INVALID";
+    }
+
+    void OnTriggerEnter2D(Collider2D col) {
+        if (col.CompareTag("Player")) {
+            if (SceneManager.GetSceneByBuildIndex(activeSceneIndex + 1).IsValid() ) {
+                SceneManager.LoadScene(activeSceneIndex + 1);
+            } else {
+                SceneManager.LoadScene(0);
+            }
+        }
+    }
+}
