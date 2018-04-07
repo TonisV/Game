7aafd992690ed48afa7aec7e0ad137edc1e38180
Unknown <ristotakk@hotmail.com>
2018-04-07 18:09:44 +0300

    Elude tekst asendatud südametega ja uus taust

--- Game - RIF16/Assets/Scripts/Menus/HUD.cs
+++ Game - RIF16/Assets/Scripts/Menus/HUD.cs
@@ -5,16 +5,22 @@ using UnityEngine.UI;
 
 public class HUD : MonoBehaviour {
 
-    private Text HealthText;
+    private Image healthHearts;
+    private float waitTime = 5.0f;
+    private float targetFillAmount;
 
     // Use this for initialization
     void Start () {
-        HealthText = GameObject.Find("HealthText").GetComponent<Text>();
+        healthHearts = GameObject.Find("HealthHearts").GetComponent<Image>();
 
     }
 	
 	// Update is called once per frame
 	void Update () {
-        HealthText.text = "Health " + PlayerController.curHealth + "/5";
+        targetFillAmount = PlayerController.curHealth * 0.2f;
+
+        if (healthHearts.fillAmount > targetFillAmount) {
+            healthHearts.fillAmount -= 1.0f / waitTime * Time.deltaTime;
+        }
     }
 }
