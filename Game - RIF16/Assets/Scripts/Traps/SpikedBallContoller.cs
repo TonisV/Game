6aca09dd0b016d201a8b53915f9a3489b3ba9ade
Unknown <ristotakk@hotmail.com>
2018-04-30 15:39:37 +0300

    Spiked ball nüüd tapab playeri

--- Game - RIF16/Assets/Scripts/Traps/SpikedBallContoller.cs
+++ Game - RIF16/Assets/Scripts/Traps/SpikedBallContoller.cs
@@ -4,65 +4,68 @@ using UnityEngine;
 
 public class SpikedBallContoller : MonoBehaviour {
 
-	private Rigidbody2D body2d;
-	public float leftPushRange = -0.3f;
-	public float rightPushRange = 0.3f;
-	public float velocityThreshold = 90f;
-	public bool useChainBreak = false;
-	public int cravityModifier = 2;
-	public Transform rayEnd;
-	private bool playerSpotted = false;
-	public int randomNumber;
+    private Rigidbody2D body2d;
+    private PlayerController playerController;
+    private GameObject player;
+    public float leftPushRange = -0.3f;
+    public float rightPushRange = 0.3f;
+    public float velocityThreshold = 90f;
+    public bool useChainBreak = false;
+    public int cravityModifier = 2;
+    public Transform rayEnd;
+    private bool playerSpotted = false;
+    public int randomNumber;
 
-	void Start () {
-		randomNumber = Random.Range(10, 20);
-		body2d = GetComponent<Rigidbody2D>();
-		body2d.angularVelocity = velocityThreshold;
-	}
-	
-	void Update () {
-		Push();
-		Raycasting();
-		BreakChain();
-	}
+    void Start() {
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerController = player.GetComponent<PlayerController>();
 
-	// Push Rigidbody to make pendulum effect
-	public void Push() {
-		if (transform.rotation.z > 0 && transform.rotation.z < rightPushRange && (body2d.angularVelocity > 0) && body2d.angularVelocity < velocityThreshold)
-		{
-			body2d.angularVelocity = velocityThreshold;
-		} 
-		else if ( transform.rotation.z < 0 && transform.rotation.z > leftPushRange && (body2d.angularVelocity < 0) && body2d.angularVelocity > velocityThreshold * -1)
-		{
-			body2d.angularVelocity = velocityThreshold * -1;
-		}
-	}
+        randomNumber = Random.Range(10, 20);
+        body2d = GetComponent<Rigidbody2D>();
+        body2d.angularVelocity = velocityThreshold;
+    }
 
-	// Break the chain when player is randomly spotted and raise rigidbody cravity to make better collision effect
-	public void BreakChain() {
-		if (useChainBreak)
-		{
-			if (GetComponent<HingeJoint2D>())
-			{
-				if (playerSpotted)
-				{
-					randomNumber--;
-				}
-				if (randomNumber < 1)
-				{
-					GetComponent<HingeJoint2D>().breakForce = 1f;
-					GetComponent<FixedJoint2D>().breakForce = 1f;
-					body2d.gravityScale = cravityModifier;
-				}
-				
-			}
-		}
-	}
+    void Update() {
+        Push();
+        Raycasting();
+        BreakChain();
+    }
 
-	// Cast ray in choosen position and check if it is collaiding with player
-	public void Raycasting() {
-		Debug.DrawLine(body2d.position, rayEnd.position, Color.red);
-		playerSpotted = Physics2D.Linecast(body2d.position, rayEnd.position, 1 << LayerMask.NameToLayer("Player"));
-	}
-	
+    // Push Rigidbody to make pendulum effect
+    public void Push() {
+        if (transform.rotation.z > 0 && transform.rotation.z < rightPushRange && (body2d.angularVelocity > 0) && body2d.angularVelocity < velocityThreshold) {
+            body2d.angularVelocity = velocityThreshold;
+        } else if (transform.rotation.z < 0 && transform.rotation.z > leftPushRange && (body2d.angularVelocity < 0) && body2d.angularVelocity > velocityThreshold * -1) {
+            body2d.angularVelocity = velocityThreshold * -1;
+        }
+    }
+
+    // Break the chain when player is randomly spotted and raise rigidbody cravity to make better collision effect
+    public void BreakChain() {
+        if (useChainBreak) {
+            if (GetComponent<HingeJoint2D>()) {
+                if (playerSpotted) {
+                    randomNumber--;
+                }
+                if (randomNumber < 1) {
+                    GetComponent<HingeJoint2D>().breakForce = 1f;
+                    GetComponent<FixedJoint2D>().breakForce = 1f;
+                    body2d.gravityScale = cravityModifier;
+                }
+
+            }
+        }
+    }
+
+    // Cast ray in choosen position and check if it is collaiding with player
+    public void Raycasting() {
+        Debug.DrawLine(body2d.position, rayEnd.position, Color.red);
+        playerSpotted = Physics2D.Linecast(body2d.position, rayEnd.position, 1 << LayerMask.NameToLayer("Player"));
+    }
+
+    void OnTriggerEnter2D(Collider2D col) {
+        if (col.CompareTag("Player")) {
+            playerController.Die();
+        }
+    }
 }
