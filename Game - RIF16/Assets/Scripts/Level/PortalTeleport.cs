cd48770fd346abfe25fa565db0699bd9e2e2ca85
Unknown <ristotakk@hotmail.com>
2018-05-21 11:16:29 +0300

    Lisatud portaali teleporteerumise valgus

--- Game - RIF16/Assets/Scripts/Level/PortalTeleport.cs
+++ Game - RIF16/Assets/Scripts/Level/PortalTeleport.cs
@@ -6,10 +6,12 @@ using UnityEngine.SceneManagement;
 public class PortalTeleport : MonoBehaviour {
 
     public int DestinationSceneIndex;
+    public Light triggerLight;
 
     private PlayerController playerController;
     private GameObject player;
     private int activeSceneIndex;
+    private bool portalTriggered;
 
     // Use this for initialization
     void Start () {
@@ -17,10 +19,21 @@ public class PortalTeleport : MonoBehaviour {
         playerController = player.GetComponent<PlayerController>();
     }
 
+    private void Update() {
+        if (portalTriggered) {
+            if (triggerLight.range < 15) {
+                triggerLight.range += 10 * Time.deltaTime;
+            } else {
+                SceneManager.LoadScene(DestinationSceneIndex);
+                PlayerPrefs.SetInt("sceneIndex", DestinationSceneIndex);
+                PlayerPrefs.DeleteKey("health");
+            }
+        }
+    }
+
     void OnTriggerEnter2D(Collider2D col) {
         if (col.CompareTag("Player")) {
-            SceneManager.LoadScene(DestinationSceneIndex);
-            PlayerPrefs.SetInt("sceneIndex", DestinationSceneIndex);
+            portalTriggered = true;
         }
     }
 }
