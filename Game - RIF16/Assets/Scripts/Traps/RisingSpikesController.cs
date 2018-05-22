5d62184b4c1c8c8f1020a5551b9d2e50c02c230e
Tõnis Vaimann <tonisv@tlu.ee>
2018-05-22 12:23:02 +0300

    Lisatud ülestõusvate ogadega lõks

---
+++ Game - RIF16/Assets/Scripts/Traps/RisingSpikesController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RisingSpikesController : MonoBehaviour {
+	
+	public GameObject risingSpikes;
+	public float risingHeight;
+	public float risingSpeed;
+	private bool playerSpotted;
+	private Vector3 targetPosition;
+
+	void Start() {
+		targetPosition = new Vector3(risingSpikes.transform.position.x, risingSpikes.transform.position.y + risingHeight, risingSpikes.transform.position.z);
+	}
+
+	void Update()
+	{
+		if (playerSpotted)
+		{
+			RiseSpikes();
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+    {	
+		playerSpotted = true;
+    }
+
+	public void RiseSpikes() {
+		risingSpikes.transform.position = Vector3.MoveTowards(risingSpikes.transform.position, targetPosition, Time.deltaTime * risingSpeed);
+	}
+}
