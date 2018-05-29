c0fbea520ee9cafdbfb5c2ffd75088f93fba1d19
Tõnis Vaimann <tonisv@tlu.ee>
2018-05-29 15:00:22 +0300

    Parandatud funktsionaalsust

--- Game - RIF16/Assets/Scripts/Player/FadeOutEffect.cs
+++ Game - RIF16/Assets/Scripts/Player/FadeOutEffect.cs
@@ -10,27 +10,58 @@ public class FadeOutEffect : MonoBehaviour {
 	private float currentTime = 0f;
 	public float timeItTakesToFade = 0.5f;
 	private bool isFading = false;
+	public bool useCollider = false;
+	public bool fadeOutAndDestroy = false;
 
 	
 	void Start() {
+		if (objectToFade == null)
+		{
+			objectToFade = this.gameObject;
+		}
 		spriteRenderer = objectToFade.GetComponent<SpriteRenderer>();
+
 	}
 	
+
 	void Update() {
 		if(isFading){
 			currentTime += Time.deltaTime;
-				if(currentTime <= timeItTakesToFade){
-				fadeValue = 1f - (currentTime / timeItTakesToFade);
-				spriteRenderer.color = new Color(1f,1f,1f, fadeValue);
+			if(currentTime <= timeItTakesToFade){
+				if (fadeValue <= 0.1f && fadeOutAndDestroy)
+				{
+					Destroy(objectToFade);
 				}
+				else
+				{
+					fadeValue = 1f - (currentTime / timeItTakesToFade);
+					spriteRenderer.color = new Color(1f,1f,1f, fadeValue);
+				}
+			}
 		}
 	}
+
  
-     void OnTriggerEnter2D(Collider2D other) {
-        if (other.CompareTag("Player"))
-        {
-        	currentTime = 0;
-     		isFading = true;
-        }
+    void OnTriggerEnter2D(Collider2D other) {
+		if (!useCollider)
+		{
+			if (other.CompareTag("Player"))
+			{
+				currentTime = 0;
+				isFading = true;
+			} 
+		}
+    }
+
+
+	void OnCollisionEnter2D(Collision2D other) {
+		if (useCollider)
+		{
+			if (other.transform.tag == "Player")
+			{
+				currentTime = 0;
+				isFading = true;
+			}
+		}
     }
 }
