af6e6b194a9615549337f06ec33f687d3da67aca
Unknown <ristotakk@hotmail.com>
2018-06-04 14:06:03 +0300

    UI parandused/muudatused

---
+++ Game - RIF16/Assets/Scripts/Menus/stopTheFuckingAnimation.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class stopTheFuckingAnimation : MonoBehaviour {
+    public void FuckingStop() {
+        this.GetComponent<Animator>().enabled = false;
+    }
+}
